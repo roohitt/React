3298d500305e4124f087c362bcc4517658298a80
rkdsndsds <rs6310227@gmail.com>
2020-05-23 09:16:06 +0530

    basefeatures

--- src/App.js
+++ src/App.js
@@ -1,52 +1,64 @@
-//useState is a react hook to manage state in a functional component
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  //usestate returns an array with two elements: 1st element is the current state and 2nd is a function to update the state
-  //below is jut=st latest js code in which the array elements are pulled out from the returned arraya nd passed to personstate and setstate 
-  const [ personState , setPersonsState ] = useState({
+class App extends Component {
+
+  state = {
     persons : [
       {name:"Vishal", age:28},
       {name:"Shreyansh", age:29},
       {name:"Lokesh", age:29},
       {name:"Kapil", age:29}
     ]
-  });
+  }
 
-  const [otherState, setOtherState]  = useState('otherstate component');
-
-  const switchNameHandler = () => {
+  switchNameHandler = (sukhayadavname) => {
     //dont do this: this.state.persons[0].name = 'Sukha';
-    //below will overwrite the persons.
-    setPersonsState( {
+    this.setState( {
       persons : [
-        {name:"Vishal Yadav", age:28},
-        {name:"Shreyansh Mmmgain", age:29},
-        {name:"Lokesh Parmar", age:29},
-        {name:"Kapil Minda", age:29}
+        {name:sukhayadavname, age:28},
+        {name:"Shreyansh Mamgain", age:29},
+        {name:"Lokesh parmar", age:29},
+        {name:"Kapil minda", age:29}
       ] 
-    } );//takes object as an argument and sets the state (is a method in the react lib)
-  };
+    } )//takes object as an argument and sets the state (is a method in the react lib)
+  }
+
+    nameChangedHandler = (event) => {
+      this.setState(
+        {
+          persons : [
+            {name:"Vishal Yadav", age:28},
+            {name: event.target.value, age:29},
+            {name:"Lokesh parmar", age:29},
+            {name:"Kapil minda", age:29}
+          ] 
+        }
+      )
+    }
+  
+
+  render() {
+
+    const style = {
+      backgroundColor: 'Red',
+      font: 'inherit',
+      border: '1px solid blue'
+    }
 
     return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
         <p>This is a para</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personState.persons[0].name}  age={personState.persons[0].age}>My hobbies: Racing</Person>
-        <Person name={personState.persons[1].name}  age={personState.persons[1].age}>My hobbies: Cricket</Person>
-        <Person name={personState.persons[2].name}  age={personState.persons[2].age}>My hobbies: Football</Person>
-        <Person name={personState.persons[3].name}  age={personState.persons[3].age}>My hobbies: BasketBall</Person>
+        <button style={style} onClick={() => this.switchNameHandler("Sukha Vishal")}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click = {this.switchNameHandler.bind(this,"Sukha")}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[1].name}  age={this.state.persons[1].age} changed = {this.nameChangedHandler}>My hobbies: Cricket</Person>
+        <Person name={this.state.persons[2].name}  age={this.state.persons[2].age}>My hobbies: Football</Person>
+        <Person name={this.state.persons[3].name}  age={this.state.persons[3].age}>My hobbies: BasketBall</Person>
       </div>
     );
-  };
+  }
+}
 
-
-export default app;
-
-
-
-
-
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,13 @@
 import React from 'react';
+//to use the Person.css file in the Person.js we have to import it over here(inside person.js). 
+import './Person.css';
 
 const person = (props) => {
 return(
-    <div>
-        <p>I'm  {props.name} and i'm {props.age } years old/</p>
+    <div className="Person">
+        <p onClick = {props.click}>I'm  {props.name} and i'm {props.age } years old</p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}></input>
     </div>
     
 ) 
